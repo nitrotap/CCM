8d0db59920d6243fb1a9ea7b16b89e333d165b8e
nitrotap <9121718+nitrotap@users.noreply.github.com>
2021-11-10 20:37:37 -0700

    updated menu
    migrating away from mentioning clinical tests to cognition survey

--- app/src/main/java/com/ccm/MainActivity.java
+++ app/src/main/java/com/ccm/MainActivity.java
@@ -2,13 +2,25 @@ package com.ccm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static long startTime;
+    public static ArrayList<String> answers; // todo remove maybe?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void qYear(View view) {
+        // Intent intent = new Intent(this, qYear.class);
+        startTime = System.currentTimeMillis();
     }
 }
